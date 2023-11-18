d8c9ae0bf646cde0f787fb2062c8979d6ec4d93d
arbinzaman <arbinzmn@gmail.com>
2023-11-18 23:04:26 +0600

    changed mongodb URl

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const dotenv = require('dotenv').config();
-const mongoose = require('mongoose');
-const port = process.env.PORT || 5000
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
 
-const app = require("./app")
+const app = require("./app");
 
-mongoose.connect(process.env.DB_LOCAL).then(()=>{
-    console.log("Database is connected")
-})
-
+mongoose
+  .connect(
+    "mongodb+srv://hms:a1s1d1f1@cluster0.sbauxbs.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Database is connected");
+  });
 
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`)
-})
+  console.log(`App is running on port ${port}`);
+});
